39b2c1a87151c2f597d326ee120d43ea1fe92e1f
David <davidalfonsook@gmail.com>
2022-12-26 17:13:17 -0300

    Cambios en la Boton =: -Funciones para obtener operandos en la multiplicacion funcionan correctamente -Funcion multiply() no funciona

--- script.js
+++ script.js
@@ -49,19 +49,108 @@ del.addEventListener('click', function(){
 
 let equal = document.querySelector('#equal');
 
+//primero quiero buscar los * y / para multiplicar y dividir
+function operadorMult(cadena){//retorna la posicion de un operador * o /
+    for (let i = 0; i < cadena.length; i++){
+        if(cadena[i] == '*'){
+            return i;
+        }
+    }
+}
+//funcion multiplicar que agarra dos numeros y los multiplica
+function multiply(num1, num2){
+    let result = parseInt(num1) * parseInt(num2);
+    result = result.toString;
+    return result;
+}
+//funcion dividir que agarra dos numeros y los multiplica
+
+//busca y devuelve la posicion del ultimo operador antes de la posicion dada
+function buscarUltimoOperador(posicion, cadena){
+    let chota;
+    for(let i = 0; i < posicion; i++){
+        if (cadena[i] == '*' || cadena[i] == '/' || cadena[i] == '+' || cadena[i] == '-'){
+            chota = i;
+        }
+    }
+    if (chota != undefined){
+        return chota;
+    }
+}
+
 equal.addEventListener('click', function(){
-    console.log(pant.textContent[pant.textContent.length - 1]);
+    //mientras siga habiendo operadores * 
+    let hayMultiplicacion;
+
+    if (operadorMult(pant.textContent) != undefined){
+        hayMultiplicacion = true;
+    }
+    console.log(hayMultiplicacion);
+
+    while (hayMultiplicacion == true){
+        console.log("entra1");
+        let pos = operadorMult(pant.textContent);
+        //encontrar el operando1 y el operando2
+        //voy a buscar el ultimo operador antes del *
+        let ultimoOperador = buscarUltimoOperador(pos, pant.textContent);
+        let lastOperator;
+        let operador1 = [];
+        let operador2 = [];
+        console.log(ultimoOperador);
+        console.log("entra2");
+        for (let i = ultimoOperador + 1, j = 0; i < pos; i++){
+            console.log("entra3");
+            operador1[j] = pant.textContent[i];
+            console.log(operador1[j]);
+            j++;
+        }
+        console.log(operador1);
+
+        let i = pos + 1;
+        let j = 0;
+
+        while (pant.textContent[i] != '*' && pant.textContent[i] != '/' && pant.textContent[i] != '+' && pant.textContent[i] != '-' && pant.textContent[i] != undefined) {
+            console.log("entra4");
+            lastOperator = i + 1;
+            operador2[j] = pant.textContent[i];
+            j++;
+            console.log("pantTextContent[i]: ",pant.textContent[i]);
+            console.log("j: ", j);
+            i++;
+        }
+        console.log(operador2);
+
+        let multiplicacion = multiply(operador1, operador2);
+        console.log(multiplicacion);
+        let nuevaCadena;
+
+        for (let i = 0; i < ultimoOperador; i++){
+            nuevaCadena[i] = pant.textContent[i];
+        }
+
+        nuevaCadena = nuevaCadena + multiplicacion;
+
+        let ultimoSopeda;
+
+        for (let i = lastOperator; pant.textContent[i] != undefined; i++) {
+            let j = 0;
+            ultimoSopeda[j] = pant.textContent[i];
+            j++;
+        }
+
+        nuevaCadena = nuevaCadena + ultimoSopeda;
+        console.log(nuevaCadena);
+        pant.textContent = nuevaCadena;
+
+        if (operadorMult(pant.textContent != undefined)){
+            hayMultiplicacion = true;
+        }
+        else{
+            hayMultiplicacion = false;
+        }
+
+    }
 })
 
-//Evitar que el usuario ingrese mas de un operador seguido
-/* SI el ultimo caracter es un operador ENTONCES
-        MIENTRAS el usuario ingrese un operador HACER
-                borrar el ultimo caracter
-                enviar un mensaje
-        FMIENTRAS
-    FSI 
-*/
-
-
-
-//una forma de hacer las operaciones es guardar los numeros de la pantalla cuando se presiona algun boton de operacion
+//si el ultimo caracter es un operador entonces llamo a la funcion (* / + -) que tome la cadena anterior  y que termine
+//de ejecutarse cuando se ingrese otro caracter
